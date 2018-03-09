ccd2b0de9227e540ecd1eef171264ac19df0adcc
Zachary Hill <zac@myth.zone>
2018-03-09 06:28:25 -0600

    Enabled speaker

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1489,7 +1489,7 @@
 // If you have a speaker that can produce tones, enable it here.
 // By default Marlin assumes you have a buzzer with a fixed frequency.
 //
-//#define SPEAKER
+#define SPEAKER
 
 //
 // The duration and frequency for the UI feedback sound.
@@ -1498,7 +1498,7 @@
 // Note: Test audio output with the G-Code:
 //  M300 S<frequency Hz> P<duration ms>
 //
-//#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 2
+#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 5
 //#define LCD_FEEDBACK_FREQUENCY_HZ 5000
 
 //
