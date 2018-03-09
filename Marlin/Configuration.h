ede41e58a5f56fb5831b2ed6a529941eeb15e2e1
Zachary Hill <zac@myth.zone>
2018-03-09 06:24:40 -0600

    Patched X_MIN_POS for my device

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -861,7 +861,7 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -30	// (MODIFIED)
+#define X_MIN_POS -35
 #define Y_MIN_POS -10
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
