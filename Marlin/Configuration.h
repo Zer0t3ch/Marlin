2de4f7363e6aaed4054855049facb621a756bc78
Zachary Hill <zac@myth.zone>
2018-03-13 01:45:15 -0500

    Reduced maximum points in UBL for faster probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1038,7 +1038,7 @@
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
   #define MESH_INSET 50              // Mesh inset margin on print area
-  #define GRID_MAX_POINTS_X 5        // Don't use more than 15 points per axis, implementation limited.
+  #define GRID_MAX_POINTS_X 4        // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
