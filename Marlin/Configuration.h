2282e3b275ad72b6185039bd3ac21e5fac78e23e
Zachary Hill <zac@myth.zone>
2018-03-28 08:05:40 -0500

    Explicitly declared Z_AFTER_PROBING

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -789,7 +789,7 @@
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
 #define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
-//#define Z_AFTER_PROBING           5 // Z position after probing is done
+#define Z_AFTER_PROBING            5 // Z position after probing is done
 
 #define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
 
